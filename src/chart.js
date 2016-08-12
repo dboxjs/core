d346ada697a994dc8d974eb0fa458c73cb411d5e
Blanca Azucena López <blanca.lopez@data4.mx>
2016-08-12 14:25:32 -0500

    Update time scales domains

--- src/chart.js
+++ src/chart.js
@@ -4,7 +4,7 @@ function queryCarto(config, callback){
 		.done(function(data){
 
 			var result = data.rows
-			//console.log('queryCarto',result);
+			console.log('queryCarto',result);
 
 			if( config.parser ){
 				result = data.rows.map(config.parser)
@@ -247,7 +247,9 @@ Chart.prototype = chart.prototype = {
 				break;
 
 				case 'time':
-					
+          minMax = d3.extent(data, function(d) { return d.x; })
+          domains.x = minMax;
+          
 				break;
 
 				case 'ordinal':
@@ -304,7 +306,8 @@ Chart.prototype = chart.prototype = {
 				break;
 
 				case 'time':
-					
+					minMax = d3.extent(data, function(d) { return d.y; })
+          domains.y = minMax;
 				break;
 
 				case 'ordinal':
