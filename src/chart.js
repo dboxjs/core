f1c0d52fd854d273714694e8807303d4b3a397f5
Jorge Andrade <jorandradefig@gmail.com>
2016-09-02 09:10:01 -0500

    style to legend in chart

--- src/chart.js
+++ src/chart.js
@@ -59,12 +59,12 @@ Chart.prototype = chart.prototype = {
 				.attr("class", "chart-legend-top");
 		//Add Legend to the chart
 		if(vm._config.legend && vm._config.legend.enable === true && vm._config.legend.position === 'top'){
-			var html = ''; 
-			html +="<div style='' class='dbox-legend-category-title'>";
+			var html = '';
+			html+="<div style='background-color:#E2E2E1;text-align:center;height: 20px;margin: 0px 15px'>";
 			vm._config.legend.categories.forEach(function(c){
-				html+="<span class='dbox-legend-category-color' style='background-color:"+c.color+";'> </span><span>"+c.title+"</span>";
+				html+="<div class='dbox-legend-category-title'><span class='dbox-legend-category-color' style='background-color:"+c.color+";'> </span><span>"+c.title+"</span></div>";
 			})
-			html+= "</div>";
+			html+="</div>";
 			legend.html(html)
 		}
 
