1e29c62a170be7460e00a458dfd88994511887d4
Alecs <alecs.garza@akora.mx>
2017-02-25 12:58:19 -0600

    Cleaning up

--- rollup.config.pb.js
+++
@@ -1,9 +0,0 @@
-import npm from "rollup-plugin-node-resolve";
-
-export default {
-  entry: "index.js",
-  format: "umd",
-  moduleName: "dbox",
-  plugins: [npm({jsnext: true})],
-  dest: "../frente-pobreza-mapa/src/dbox/build/dbox.js"
-};
