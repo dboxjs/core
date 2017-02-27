020c822d67103f3a03cd0b8afda9d98c5af7c0be
Alecs <alecs.garza@akora.mx>
2017-02-27 09:39:14 -0600

    configurations

--- rollup.config.dev.js
+++
@@ -1,9 +0,0 @@
-import npm from "rollup-plugin-node-resolve";
-
-export default {
-  entry: "index.js",
-  format: "umd",
-  moduleName: "dbox",
-  plugins: [npm({jsnext: true})],
-  dest: "../dbox-website/src/app/pkg-dbox-examples/js/dbox.js"
-};
