dd8d10e951e3a1707603a55a0ef86af6ab03e30d
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2017-03-08 20:49:52 -0600

    Adding tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,35 @@
 import * as dbox from '../';
 import * as assert from 'assert';
 
-describe('dbox', () => {
-  it('creates a chart template', () => {
+describe('chart core', () => {
+  it('Creates a default chart config template', () => {
     var chart = dbox.chart();
-    assert.deepEqual(chart._config, {size: {}});
+    assert.deepEqual(chart._config, {size: {width: 800, height: 600, margin: {left: 0, right: 0, top: 0, bottom: 0}}});
+  });
+
+  it('Should set size options using chained method', () => {
+    var size = {
+      width: 400,
+      height: 400,
+      margin: {
+        top: 0,
+        right: 10,
+        bottom: 5,
+        left: 10
+      }
+    };
+    var chart = dbox.chart()
+            .size(size);
+
+    assert.deepEqual(chart._config.size, size);
+    assert.deepEqual(chart._width, size.width);
+    assert.deepEqual(chart._height, size.height);
+    assert.deepEqual(chart._margin, size.margin);
+  });
+
+  it('Should bind chart to element', () => {
+    var chart = dbox.chart()
+                .bindTo('body')
+    assert.strictEqual(chart._config.bindTo, 'body');
   });
 });
