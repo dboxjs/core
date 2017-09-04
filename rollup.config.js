70e175cd150edb2a06e1f985a9c06507c19c8a5f
Jorge Andrade <jorandradefig@gmail.com>
2017-09-04 05:51:00 -0500

    refactor(rollup.config.js): add external-helpers

--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,23 @@ import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 
 import istanbul from 'rollup-plugin-istanbul';
-import * as _ from 'lodash';
-import * as d3 from 'd3';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
-//plugins: [
-    //babel(babelrc()),
-    //istanbul({
-      //exclude: ['test/**/*', 'node_modules/**/*', 'docs/**/*', '.editorconfig']
-    //})
-  //],
-
 export default {
-  entry: 'lib/index.js',
+  entry: 'index.js',
   plugins: [
     resolve(),
+    'external-helpers',
     babel(babelrc())
   ],
   external: external,
   globals: {
-    lodash: '_',
-    d3:'d3',
+    'lodash': '_',
+    'd3':'d3',
+    'cartodb':'cartodb',
+    'textures':'textures'
   },
   targets: [
     {
@@ -36,12 +30,3 @@ export default {
     }
   ]
 };
-
-/*
-,
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-*/
