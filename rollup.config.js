ee9cebbe0339f2a32bdc778488b8d846441e8995
Blanca Azucena López Garduño <blanca.lopez@data4.mx>
2018-04-12 13:35:40 -0500

    Show tip to display large names on legend and axes

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
+// import istanbul from 'rollup-plugin-istanbul';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
