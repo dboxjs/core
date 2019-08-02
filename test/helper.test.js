774d764fcb21a7bb73bcb6ddbcc50f48273267c1
Blanca Azucena López Garduño <blanca.lopez@data4.mx>
2019-08-02 15:13:09 -0500

    Update utils.format funcion based on config object. Now returns a funcion to format d value

--- test/helper.test.js
+++ test/helper.test.js
@@ -5,47 +5,47 @@ describe('Helper.utils', function() {
   describe('format()', function() {
     it('Should format 5450000 to 5.5 millones', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(5450000, true);
+      var result = chart.helper.utils.format(null, true)(5450000);
       assert.equal(result, '5.5 millones');
     });
     it('Should format 430958 to 431.0 mil', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(430958, true);
+      var result = chart.helper.utils.format(null, true)(430958);
       assert.equal(result, '431.0 mil');
     });
     it('Should format 6234 to 6.2 mil', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(6234, true);
+      var result = chart.helper.utils.format(null, true)(6234);
       assert.equal(result, '6.2 mil');
     });
     it('Should format 345.23 to 345.2', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(345.23, true);
+      var result = chart.helper.utils.format(null, true)(345.23);
       assert.equal(result, '345.2');
     });
     it('Should format 5.2093 to 5.2', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(5.2093, true);
+      var result = chart.helper.utils.format(null, true)(5.2093);
       assert.equal(result, '5.2');
     });
     it('Should format 12.0 to 12', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(12.0, true);
+      var result = chart.helper.utils.format(null, true)(12.0);
       assert.equal(result, '12');
     });
     it('Should format 1.4904 to 1.5', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(1.4904, true);
+      var result = chart.helper.utils.format(null, true)(1.4904);
       assert.equal(result, '1.5');
     });
     it('Should format 0.4935 to 0.5 ', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(0.4935, true);
+      var result = chart.helper.utils.format(null, true)(0.4935);
       assert.equal(result, '0.5');
     });
     it('Should format 0.00435 to 0.004 ', function() {
       var chart = dbox.chart();
-      var result = chart.helper.utils.format(0.00435, true);
+      var result = chart.helper.utils.format(null, true)(0.00435);
       assert.equal(result, '0.004');
     });
   });
