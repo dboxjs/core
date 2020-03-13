59dc107e3426c6479927b5f12d4430d196f8ab95
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2020-03-13 13:57:31 -0600

    test: Remove reporter istanbul

--- test/istanbul.reporter.js
+++ test/istanbul.reporter.js
@@ -1,16 +1,16 @@
 const instanbul = require('istanbul');
 const MochaSpecReporter = require('mocha/lib/reporters/spec');
 
-module.exports = function (runner) {
+module.exports = function(runner) {
   const collector = new instanbul.Collector();
   const reporter = new instanbul.Reporter();
   reporter.addAll(['lcov', 'json']);
   new MochaSpecReporter(runner);
 
-  runner.on('end', function () {
+  runner.on('end', function() {
     collector.add(global.__coverage__);
 
-    reporter.write(collector, true, function () {
+    reporter.write(collector, true, function() {
       process.stdout.write('report generated');
     });
   });
