d2ccb3f4511cbd86cbe6d05a89829d7a49020eb2
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2020-03-13 13:08:46 -0600

    feat: Add distro and leaflet, format code

--- index.js
+++ index.js
@@ -8,39 +8,25 @@
  */
 
 /* Core */
-export {
-  default as chart,
-} from './lib/chart';
+export { default as chart } from './lib/chart';
 
 /* Chart modules */
-export {
-  default as bars
-} from '@dboxjs/bars';
+export { default as bars } from '@dboxjs/bars';
 
-export {
-  default as spineplot
-} from '@dboxjs/spineplot';
+export { default as distro } from '@dboxjs/distro';
 
-export {
-  default as heatmap,
-} from '@dboxjs/heatmap';
+export { default as heatmap } from '@dboxjs/heatmap';
 
-export {
-  default as radar
-} from '@dboxjs/radar';
+export { default as leaflet } from '@dboxjs/leaflet';
 
-export {
-  default as scatter,
-} from '@dboxjs/scatter';
+export { default as map } from '@dboxjs/map';
 
-export {
-  default as timeline,
-} from '@dboxjs/timeline';
+export { default as radar } from '@dboxjs/radar';
 
-export {
-  default as treemap,
-} from '@dboxjs/treemap';
+export { default as scatter } from '@dboxjs/scatter';
 
-export {
-  default as map,
-} from '@dboxjs/map';
+export { default as spineplot } from '@dboxjs/spineplot';
+
+export { default as timeline } from '@dboxjs/timeline';
+
+export { default as treemap } from '@dboxjs/treemap';
