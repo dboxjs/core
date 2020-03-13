6fff727b75d3be1fa888d4858e19414162262759
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2020-03-13 13:56:58 -0600

    refactor: Update tests names

--- test/chart.test.js
+++ test/chart.test.js
@@ -2,7 +2,7 @@ import * as dbox from '../';
 import * as assert from 'assert';
 
 describe('Chart', () => {
-  it('Should be defined on dbox', () => {
+  it('Chart should be defined on dbox', () => {
     assert.ok(dbox.chart);
   });
 });
--- test/scatter.test.js
+++ test/scatter.test.js
@@ -2,7 +2,8 @@ import * as dbox from '../';
 import * as assert from 'assert';
 
 describe('Scatter chart', () => {
-  it('Should be defined on dbox', () => {
+  it('Scatter should be defined on dbox', () => {
+    console.log(dbox.scatter);
     assert.ok(dbox.scatter);
   });
 });
