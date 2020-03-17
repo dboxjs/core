7eb469835867134f8edd962d7a6abd480dec7600
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2020-03-17 12:22:28 -0600

    fix: Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,29 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
-// import istanbul from 'rollup-plugin-istanbul';
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
 export default {
   input: 'index.js',
-  plugins: [resolve(), 'external-helpers', babel(babelrc())],
+  plugins: [
+    resolve(),
+    'external-helpers',
+    babel(babelrc())
+  ],
   external: external,
-  globals: {
-    lodash: '_',
-    d3: 'd3',
-    textures: 'textures'
-  },
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      name: 'dbox',
-      sourcemap: true
-    }
-  ]
+  output:
+  {
+    file: pkg.main,
+    format: 'umd',
+    name: 'dbox',
+    sourcemap: true,
+    globals: {
+      'lodash': '_',
+      'd3': 'd3',
+      'cartodb': 'cartodb',
+      'textures': 'textures'
+    },
+  }
 };
